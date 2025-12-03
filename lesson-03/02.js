5302a999b8ab57816ec6886225a3bfac53781984
Ilya Gaidukevich <ilyaishisname@gmail.com>
2025-12-03 23:05:09 +0300

    Update 02.js

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,6 +1,10 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
-let num = Math.ceil(Math.random() * 10);
+let num = Math.floor(Math.random() * 10);
+if (num === 0) {
+    num = -Math.ceil(Math.random() * 10)
+}
+
 console.log(num);
 
 function isEven(num) {
