7ae87c331d9903e5adb532635c241b22e04f0665
Ilya Gaidukevich <ilyaishisname@gmail.com>
2025-12-04 22:46:50 +0300

    Update 03.js

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,18 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
+let num1 = Math.ceil(Math.random() * 10)
+let num2 = Math.ceil(Math.random() * 10)
+let num3 = Math.ceil(Math.random() * 10)
+console.log(num1, num2, num3)
 
-function findLargest() {}
+function findLargest(num1, num2, num3) {
+    if (num1 > num2 && num1 > num3) {
+        return num1
+    } else if (num2 > num1 && num2 > num3) {
+        return num2
+    } else {
+        return num1 || num2
+    }
+}
+
+findLargest(num1, num2, num3);
+
