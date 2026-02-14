922d966027d442e4434f8b746cbe4ee956fd26f2
Ilya Gaidukevich <ilyaishisname@gmail.com>
2026-02-14 15:09:05 +0300

    Update 02.js

--- lesson-06/02.js
+++ lesson-06/02.js
@@ -24,4 +24,11 @@ const doubledNumbers = map(numbers, (element, index) => {
 console.log(doubledNumbers) // Должен вывести: [2, 4, 6, 8, 10]
 */
 
-const map = () => {}
+let numbers = [1, 2, 3, 4, 5];
+
+const map = numbers.map((element, index) => {
+  return element * 2
+})
+
+console.log(map);
+console.log(numbers);
